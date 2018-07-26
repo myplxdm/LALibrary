91db9a5464d5c720b7000a91fab892c42a068096
liu <liu@liudeAir.lan>
2018-07-26 12:01:43 +0800

    no message

--- ALibraryTest/lalibrary/src/main/java/com/liu/app/version/VersionManager.java
+++ ALibraryTest/lalibrary/src/main/java/com/liu/app/version/VersionManager.java
@@ -13,6 +13,7 @@ import com.liu.lalibrary.AbsActivity;
 import com.liu.lalibrary.log.LogUtils;
 import com.liu.lalibrary.ui.AlertDialogHelper;
 import com.liu.lalibrary.update.UpdateManager;
+import com.liu.lalibrary.utils.AppUtils;
 import com.liu.lalibrary.utils.PermissionsUtil;
 
 /**
@@ -52,19 +53,23 @@ public class VersionManager
                         if (nr.state == 0)
                         {
                             final VersionInfo vi = JSON.parseObject(nr.obj, VersionInfo.class);
+                            final AbsActivity activity = listener.onVMGetActivity();
+                            if (activity == null)return;
                             listener.onRecvVersion(vi);
-                            if (!TextUtils.isEmpty(vi.remark) || vi.enforce)
+                            String ver = AppUtils.getVersionName(activity);
+                            if (compareVersion(vi.ver, ver) == 1)
                             {
-                                final int type = listener.onGetUpdateType(vi);
-                                final AbsActivity activity = listener.onVMGetActivity();
-                                switch (type)
+                                if (!TextUtils.isEmpty(vi.remark) || vi.enforce)
                                 {
-                                    case VM_TYPE_NONE:
-                                        break;
-                                    case VM_TYPE_AUTO:
-                                    case VM_TYPE_LOCK:
-                                        if (activity != null)
-                                        {
+                                    final int type = listener.onGetUpdateType(vi);
+                                    if (activity == null) return;
+                                    switch (type)
+                                    {
+                                        case VM_TYPE_NONE:
+                                            break;
+                                        case VM_TYPE_AUTO:
+                                        case VM_TYPE_LOCK:
+
                                             if (TextUtils.isEmpty(vi.remark))
                                             {
                                                 vi.remark = "该版本太低，需强制升级到最新版本";
@@ -77,14 +82,8 @@ public class VersionManager
                                                     confirmUpdate(activity, vi.url, vi.remark, vi.enforce, type == VM_TYPE_LOCK);
                                                 }
                                             });
-                                        }
-                                        break;
-                                }
-
-
-                                if (activity != null)
-                                {
-
+                                            break;
+                                    }
                                 }
                             }
                         }
@@ -101,6 +100,35 @@ public class VersionManager
 
             }
         });
+    }
+
+    /**
+     * 比较版本号的大小,前者大则返回一个正数,后者大返回一个负数,相等则返回0
+     * @param version1
+     * @param version2
+     * @return
+     */
+    public static int compareVersion(String version1, String version2)
+    {
+        if (version1 == null || version2 == null)
+        {
+            return 0;
+        }
+
+        String[] versionArray1 = version1.split("\\.");//注意此处为正则匹配，不能用"."；
+        String[] versionArray2 = version2.split("\\.");
+        int idx = 0;
+        int minLength = Math.min(versionArray1.length, versionArray2.length);//取最小长度值
+        int diff = 0;
+        while (idx < minLength
+                && (diff = versionArray1[idx].length() - versionArray2[idx].length()) == 0//先比较长度
+                && (diff = versionArray1[idx].compareTo(versionArray2[idx])) == 0)
+        {//再比较字符
+            ++idx;
+        }
+        //如果已经分出大小，则直接返回，如果未分出大小，则再比较位数，有子版本的为大；
+        diff = (diff != 0) ? diff : versionArray1.length - versionArray2.length;
+        return diff;
     }
 
     public void update(final AbsActivity activity, final String url)
