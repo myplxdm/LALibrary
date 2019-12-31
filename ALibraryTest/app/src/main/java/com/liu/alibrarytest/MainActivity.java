e72091ef59b2e001ec13bc645195c884ab0cdba6
jinhui liu <>
2019-12-31 09:54:29 +0800

    no message

--- ALibraryTest/app/src/main/java/com/liu/alibrarytest/MainActivity.java
+++ ALibraryTest/app/src/main/java/com/liu/alibrarytest/MainActivity.java
@@ -2,19 +2,12 @@ package com.liu.alibrarytest;
 
 import android.content.Intent;
 import android.os.Build;
-import android.os.Bundle;
 import android.support.annotation.RequiresApi;
-import android.view.LayoutInflater;
-import android.view.View;
 import android.widget.Button;
-import android.widget.RadioButton;
-import android.widget.Toast;
 
 import com.liu.app.pluginImpl.PluginContacts;
+import com.liu.app.ui.TBButton;
 import com.liu.lalibrary.AbsActivity;
-import com.liu.lalibrary.ui.FlowLayout;
-
-import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -26,8 +19,8 @@ public class MainActivity extends AbsActivity
 
     @BindView(R.id.add)
     Button add;
-    @BindView(R.id.fl)
-    FlowLayout fl;
+    @BindView(R.id.tb)
+    TBButton tb;
 
     @Override
     protected int getRootViewId()
@@ -40,24 +33,30 @@ public class MainActivity extends AbsActivity
     protected void onInitView()
     {
         ButterKnife.bind(this);
-        LayoutInflater li = LayoutInflater.from(this);
-        TagButton tb;
-        for (int i = 0;i < 3;i++)
-        {
-            tb = new TagButton(this);
-            tb.setText("test " + i);
-            fl.addView(tb);
-        }
-        fl.setFlowItemListener(new FlowLayout.FlowItemListener()
-        {
-            @Override
-            public void onFlowSelItem(View child, boolean isSel, int index)
-            {
-                ((RadioButton) child).setChecked(isSel);
-                //String ss = String.format("%s %d", ((RadioButton) child).getText().toString(), isSel ? 1 : 0);
-                //Toast.makeText(MainActivity.this, ss, Toast.LENGTH_LONG).show();
-            }
-        });
+
+//        fl.setFlowItemListener(new FlowLayout.FlowItemListener()
+//        {
+//            @Override
+//            public void onFlowSelItem(View child, boolean isSel, int index)
+//            {
+//                ((RadioButton) child).setChecked(isSel);
+//                if (!isSel)
+//                {
+////                    fl.removeView(child);
+//                }
+//                //String ss = String.format("%s %d", ((RadioButton) child).getText().toString(), isSel ? 1 : 0);
+//                //Toast.makeText(MainActivity.this, ss, Toast.LENGTH_LONG).show();
+//            }
+//        });
+//        LayoutInflater li = LayoutInflater.from(this);
+//        TagButton tb;
+//        for (int i = 0;i < 3;i++)
+//        {
+//            tb = new TagButton(this);
+//            tb.setText("test " + i);
+//            fl.addView(tb, true);
+//        }
+
     }
 
     @Override
@@ -69,7 +68,6 @@ public class MainActivity extends AbsActivity
     @OnClick(R.id.add)
     public void onViewClicked()
     {
-        ArrayList list = fl.getSelectList();
-        toast("size = " + (list == null ? 0 : list.size()));
     }
+
 }
